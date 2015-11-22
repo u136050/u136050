179fe7e3f963a46a6b4ca3352759fe0e2de8072a
Nikita Pandykly <w-nekit-p@mail.ru>
2015-11-22 14:25:44 +0300

    Auth site template have been changed

---
+++ local/templates/login/lang/ru/header.php
@@ -0,0 +1,13 @@
+<?
+$MESS["BITRIX24_MOTTO"] = "Социальный интранет";
+$MESS["BITRIX24_URL"] = "http://www.bitrix24.ru";
+$MESS["BITRIX24_TITLE"] = "Bitrix24";
+$MESS["BITRIX24_LANG_INTERFACE"] = "Язык";
+$MESS["BITRIX24_LANG_RU"] = "Русский";
+$MESS["BITRIX24_LANG_EN"] = "Английский";
+$MESS["BITRIX24_LANG_DE"] = "Немецкий";
+$MESS["BITRIX24_LANG_UA"] = "Украинский";
+$MESS["BITRIX24_LANG_LA"] = "Испанский";
+$MESS["BITRIX24_COPYRIGHT"] = "&copy; #CURRENT_YEAR#";
+$MESS["BITRIX24_COPYRIGHT_B24"] = "&copy; #CURRENT_YEAR#";
+?>
